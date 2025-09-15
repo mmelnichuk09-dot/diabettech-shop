2229af6a271138ef3e17451702361bc3798aa659
Yevheniy Melnychuk <mmelnichuk09@gmail.com>
2025-09-15 11:37:45 +0100

    Switch database to PostgreSQL (Render)

--- freestyle_shop_test/settings.py
+++ freestyle_shop_test/settings.py
@@ -4,6 +4,7 @@ Django settings for freestyle_shop_test project.
 
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,10 +60,7 @@ WSGI_APPLICATION = 'freestyle_shop_test.wsgi.application'
 # Database
 # Для начала SQLite (локально и на Render для теста)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 # Если захочешь PostgreSQL (Render предоставляет), можно включить psycopg2:
